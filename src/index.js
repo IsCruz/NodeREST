0975d41d04e4a6069a6c9f39c0c2b6a3264d6105
Israel Cruz <israelcruzibarra@gmail.com>
2018-09-25 20:40:32 -0700

    Add started files (setup, routes and views)

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+const routes = require('./routes/index.js');
+const routest = require('./routes/task.js');
+
+
+//setting port
+
+app.engine('html', require('ejs').renderFile);
+app.set('views',path.join(__dirname,'views'));
+app.set('port', process.env.PORT || 3000);
+app.set('view engine','ejs');
+
+
+//middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+//routes
+app.use(routes);
+app.use('/api',routest);
+
+
+app.listen(app.get('port'), () => {
+   console.log('server on port 3000');
+});
